4e3979637713485cb51d324f78af188ebff31e54
Kim Changgyu <kimc980106@naver.com>
2022-11-30 15:12:27 +0900

    Initial Commit

---
+++ src/main/java/com/example/kreambidding/KreamBiddingApplication.java
@@ -0,0 +1,13 @@
+package com.example.kreambidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreamBiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreamBiddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kreambidding/KreamBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kreambidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreamBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
