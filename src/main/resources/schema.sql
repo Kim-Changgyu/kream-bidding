7909529ab62b92ac8193b58627266be9853ffd2a
Kim Changgyu <kimc980106@naver.com>
2022-12-04 20:45:38 +0900

    Feat: 데이터베이스 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE if NOT EXISTS user
 (
     id          BIGINT      PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name        VARCHAR(50) NOT NULL,
+    email       VARCHAR(50) NOT NULL UNIQUE,
     address     VARCHAR(50) NOT NULL
 );
 
@@ -17,4 +18,25 @@ CREATE TABLE if NOT EXISTS product
     released_at      DATETIME(6),
     gender          VARCHAR(20),
     size            VARCHAR(20)
+);
+
+CREATE TABLE if NOT EXISTS bidding
+(
+    id              BIGINT          PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    product_id      VARCHAR(50)     NOT NULL,
+    user_id         VARCHAR(50)     NOT NULL,
+    bidding_type    VARCHAR(50)     NOT NULL,
+    price           BIGINT          NOT NULL,
+    bidding_status  VARCHAR(50)     NOT NULL,
+    created_at      DATETIME(6)     DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE if NOT EXISTS trade
+(
+    id              BIGINT          PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    buy_bidding_id  VARCHAR(50)     NOT NULL,
+    sell_bidding_id VARCHAR(50)     NOT NULL,
+    invoice_number  VARCHAR(50)     DEFAULT NULL,
+    price           BIGINT          NOT NULL,
+    trade_status    VARCHAR(50)     NOT NULL
 );
